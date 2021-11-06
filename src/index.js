86a80317a29cea71ec21597201e0fd6dad25c25b
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-06 11:21:02 +0600

    added context api

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { GithubProvider } from './context/context';
-import { Auth0Provider } from '@auth0/auth0-react';
+import GithubProvider from './context/context';
+import Auth0Provider from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <GithubProvider>
     <App />
-  </React.StrictMode>,
+  </GithubProvider>,
   document.getElementById('root')
 );
